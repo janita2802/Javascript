5a2e94d46ada5d3f5af3ffe7d825c5419504e007
Janita_Phatak <44046907+janita2802@users.noreply.github.com>
2024-09-24 08:43:58 +0000

    Explore datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; //treat all js code as newer version.
+// alert("hello"); //We are using node.js , not browser.
+
+// tc39 & Mdn ---js doc
+
+console.log(3 
+    +
+     3) // wrong way, code readability should be high
+
+console.log("janita")
+
+
+let name = "janita"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value is not assigned but variable is declared.
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
