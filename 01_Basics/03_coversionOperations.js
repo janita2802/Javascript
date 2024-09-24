53a97e5aa6f1a4d2cb9640f6121cd9365b5021d2
Janita_Phatak <44046907+janita2802@users.noreply.github.com>
2024-09-24 12:41:22 +0000

    Explore diffrent operations on variable.

--- 01_Basics/03_coversionOperations.js
+++ 01_Basics/03_coversionOperations.js
@@ -26,3 +26,56 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
+let value = 5
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+
+let str1 = "hello"
+let str2 = " janita"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log( (3 + 4) * 5 % 3);
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+console.log(num3);
+
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`); // Expected output: "x:4, y:3
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`); // Expected output: "a:4, b:4"
