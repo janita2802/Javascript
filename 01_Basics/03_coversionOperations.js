c289670235e9fed2e073bb8a1cad252cba51529d
Janita_Phatak <44046907+janita2802@users.noreply.github.com>
2024-09-24 12:34:29 +0000

    Discuss Coversion of datatypes

---
+++ 01_Basics/03_coversionOperations.js
@@ -0,0 +1,28 @@
+let score = "Janita"
+
+console.log(typeof score);
+console.log(typeof(score)); //another way using method
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "Janita"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Janita" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
